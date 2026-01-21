2bcf62a487e6e25e5de16ad257f327ccb3519bb1
Darker_DG <darkeryt20@gmail.com>
2026-01-21 22:05:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import discord
 import os
 from discord.ext import commands
 
-# Bot ට අවශ්‍ය Permissions (Intents) සැකසීම
 intents = discord.Intents.default()
 intents.message_content = True
 
@@ -11,12 +10,22 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user.name}')
+    # ඔයා ලබාදුන් Channel ID එක මෙතනට දැම්මා
+    channel_id = 1463499215954247711 
+    channel = bot.get_channel(channel_id)
+    if channel:
+        await channel.send("Bot එක දැන් Koyeb හරහා Online ආවා! 🚀")
+    else:
+        print("Channel එක හොයාගන්න බැරි වුණා. ID එක හරිද කියලා බලන්න.")
 
 @bot.command()
 async def hello(ctx):
     await ctx.send('Hello there! I am online.')
 
-
+# Koyeb Environment Variable එකෙන් Token එක ලබා ගැනීම
 token = os.getenv('DISCORD_TOKEN')
-bot.run(token)
 
+if token:
+    bot.run(token)
+else:
+    print("Error: DISCORD_TOKEN කියන Environment Variable එක Koyeb වල දාලා නැහැ!")
