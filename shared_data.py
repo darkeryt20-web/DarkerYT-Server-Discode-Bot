aca61c08c34234e413710c4a72b3ece56b4e5d48
Darker_DG <darkeryt20@gmail.com>
2026-02-26 13:18:22 +0530

    Create shared_data.py

---
+++ shared_data.py
@@ -0,0 +1,22 @@
+# shared_data.py
+bot_stats = {
+    "status": "Offline",
+    "servers": 0,
+    "members": 0,
+    "latency": "0ms",
+    "uptime": "0",
+    "last_update": "Never"
+}
+
+verify_stats = {
+    "name": "Verification Bot",
+    "status": "Offline",
+    "last_run": "Never"
+}
+
+welcome_stats = {
+    "name": "Welcomer Bot",
+    "status": "Offline",
+    "welcomes_sent": 0,
+    "last_welcome": "None"
+}
