997b08fabfa334ed102d0aa71e20d2ded194ff88
ᴅᴀʀᴋ ᴄᴏꜱᴍɪᴄ <darkeryt20@gmail.com>
2026-07-11 20:17:40 +0530

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# අතිරේක extension/web service එක ක්‍රියාත්මක කරන්න (background එකේ)
+gunicorn -b 0.0.0.0:${PORT:-8000} Verification:app &
+
+# Discord Bot එක ක්‍රියාත්මක කරන්න
+python Verification.py
