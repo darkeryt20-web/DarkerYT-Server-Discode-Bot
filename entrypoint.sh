3cef3b90c935a8a37d8491700c15ce95737d7bcf
ᴅᴀʀᴋ ᴄᴏꜱᴍɪᴄ <darkeryt20@gmail.com>
2026-07-11 20:19:41 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # අතිරේක extension/web service එක ක්‍රියාත්මක කරන්න (background එකේ)
-gunicorn -b 0.0.0.0:${PORT:-8000} Verification:app &
+gunicorn -b 0.0.0.0:${PORT:-8000} CDTG:app &
 
 # Discord Bot එක ක්‍රියාත්මක කරන්න
 python Verification.py
